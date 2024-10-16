de6def94e183d9583403452d8ae9a3934505a661
patrickeraa <patrick.araujo@ufv.br>
2024-10-16 00:19:17 -0300

    Character class creation.

--- untitled/src/main/java/org/example/Character.java
+++ untitled/src/main/java/org/example/Character.java
@@ -1,8 +1,11 @@
+package org.example;
+
 public class Character {
     String name, classe;
     int life, maxLife, attack, defense;
     public Character(String name, String classe, int life, int maxLife, int attack, int defense ) {
         this.name = name;
+        this.classe = classe;
         this.life = life;
         this.maxLife = life;
         this.attack = attack;
@@ -10,7 +13,7 @@ public class Character {
     }
 
     public void status(){
-        System.out.println("Name: " + this.name + ", " + this.classe);
+        System.out.println("About: " + this.name + ", " + this.classe);
         System.out.println("Life: " + this.life + "/" + this.maxLife);
         System.out.println("Defense: " + this.defense);
         System.out.println("Attack: " + this.attack);
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,9 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Character warrior = new Character("Clyver", 16, 14, 16, 8, 10, 12, 1, 10, 16);
-        Character mage = new Character("Bob", 8, 14, 14, 18, 12, 10, 1, 6, 10);
-
+        Character warrior = new Character("Antonio", "Warrior", 10, 10, 10, 10);
+        Character mage = new Character("Bob", "Wizard", 10, 10, 10, 10);
         warrior.status();
         mage.status();
     }
