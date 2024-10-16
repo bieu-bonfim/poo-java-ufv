ef395de22356472fe3e8d6ecd1b109cbba028b6d
patrickeraa <patrick.araujo@ufv.br>
2024-10-16 00:15:36 -0300

    Character class creation.

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Character warrior = new Character("Clyver", 16, 14, 16, 8, 10, 12, 1, 10, 16);
+        Character mage = new Character("Bob", 8, 14, 14, 18, 12, 10, 1, 6, 10);
+
+        warrior.status();
+        mage.status();
     }
 }
